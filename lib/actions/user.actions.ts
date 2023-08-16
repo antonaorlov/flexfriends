6da1b5e6b1854840f54c68c25859f4157ee30185
Kashan Haider <19cs162@jssaten.ac.in>
2023-08-16 18:31:34 +0530

    Fixed the profile error

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,7 @@
 "use server"
 import { connectToDB } from "../mongoose"
 import User from "../models/user.model";
+import Thread from "../models/threads.model";
 import { revalidatePath } from "next/cache";
 interface params{
     
@@ -45,4 +46,29 @@ export async function fetchUser(userId:string) {
         throw new Error(`Failed to fetch user: ${err.message}`)
     }
     
+}
+
+export async function fetchUserThread(userId:string){
+    try{
+        connectToDB()
+        const threads = await User.findOne({id:userId})
+            .populate({
+                path:'threads',
+                model:Thread,
+                populate:{
+                    path:'children',
+                    model:Thread,
+                    populate:{
+                        path:'author',
+                        model:User,
+                        select:"name image id"
+                    }
+
+                }
+            })
+        return threads
+    }
+    catch(err:any){
+        throw new Error(`Failed to fetch user posts: ${err.message}`)
+    }
 }
