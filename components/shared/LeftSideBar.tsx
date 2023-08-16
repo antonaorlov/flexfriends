347c21eb92ccf4e9c65a19d687e776a44ea7779d
Kashan Haider <19cs162@jssaten.ac.in>
2023-08-16 18:22:56 +0530

    Fixed the link issue

--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -5,20 +5,23 @@ import { sidebarLinks } from "@/constants";
 import Image from 'next/image';
 import {usePathname,useRouter} from 'next/navigation'
 import { SignedIn, SignOutButton } from '@clerk/nextjs';
+import { useAuth } from '@clerk/nextjs';
 const LeftSideBar = () => {
   const router = useRouter()
   const pathname = usePathname()
+  const {userId} = useAuth()
 
   return (
     <section className='custom-scrollbar leftsidebar'>
       <div className='flex w-full flex-1 flex-col gap-6 px-6'>
         {sidebarLinks.map((link) =>{
           const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname == link.route 
+          if (link.route ==="/profile") link.route=`${link.route}/${userId}`
         return (
           <Link 
             href={link.route}
             key={link.label}
-            className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}
+            className={`leftsidebar_link ${isActive && 'bg-tertiary-500'}`}
           >
             <Image
               src={link.imgURL}
